516809c74b94fdcb0b9e8dbdec6e3d0b2fe2b958
Isaactaken <IKISEMBO01@mynbcc.ca>
2024-12-20 13:11:52 -0400

    added crud in vue accessing laravel api

--- ccs/ccs-frontend/src/router/index.js
+++ ccs/ccs-frontend/src/router/index.js
@@ -1,22 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PostView from '@/views/Post/PostView.vue'
+import CreateView from '@/views/Post/CreateView.vue'
+import ShowView from '@/views/Post/ShowView.vue'
+import UpdateView from '@/views/Post/UpdateView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/', // defines a route
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/posts',
+      name: 'posts',
+      component: PostView,
     },
+    {
+      path: '/create',
+      name: 'create',
+      component: CreateView,
+    },
+    {
+      path: '/show/:id',
+      name: 'show',
+      component: ShowView,
+    },
+    {
+      path: '/post/update/:id',
+      name: 'update',
+      component: UpdateView,
+    }
   ],
 })
 
--- ccs/ccs-frontend/src/views/HomeView.vue
+++ ccs/ccs-frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="title">Hello world</h1>
   </main>
 </template>
